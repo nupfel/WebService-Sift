dd4c21dd187ce09c6a1763ce7dbf05aa318729bd
Tobias Kirschstein <lev@ideegeo.com>
2014-09-24 12:24:43 +1200

    initial commit

---
+++ lib/WebService/Sift.pm
@@ -0,0 +1,92 @@
+package WebService::Sift;
+
+use Modern::Perl;
+use Mouse;
+
+# ABSTRACT: 
+
+# VERSION
+
+use Data::Dump;
+
+
+=head1 SYNOPSIS
+
+Quick summary of what the module does.
+
+Perhaps a little code snippet.
+
+    use Some::Module;
+    use Data::Dump 'dump';
+
+=head1 ATTRIBUTES
+
+=head2 attr
+
+=cut
+
+has 'attr' => (
+    is => 'rw',
+);
+
+
+
+=head1 SUBROUTINES/METHODS
+
+=head2 method
+
+=cut
+
+sub method {
+    my ($self) = @_;
+}
+
+
+
+=head1 BUGS
+
+Please report any bugs or feature requests on GitHub's issue tracker L<https://github.com/<github_user>/WebService::Sift/issues>.
+Pull requests welcome.
+
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc WebService::Sift
+
+
+You can also look for information at:
+
+=over 4
+
+=item * GitHub repository
+
+L<https://github.com/<github_user>/WebService::Sift>
+
+=item * MetaCPAN
+
+L<https://metacpan.org/module/WebService::Sift>
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/WebService::Sift>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/WebService::Sift>
+
+=back
+
+
+=head1 ACKNOWLEDGEMENTS
+
+=over 4
+
+=item * Lenz Gschwendtner (@norbu09), for being an awesome mentor and friend.
+
+=back
+
+=cut
+
+1;  # End of WebService::Sift
